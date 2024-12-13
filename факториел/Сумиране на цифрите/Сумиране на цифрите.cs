6657275bb10483b9b0463eb4901b0e7b1865d750
Nataliq-Vulkova <nataliavalkova2009@gmail.com>
2024-12-13 08:54:01 +0200

    Add files via upload

---
+++ факториел/Сумиране на цифрите/Сумиране на цифрите.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сумиране_на_цифрите
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            var n = int.Parse(Console.ReadLine());
+            int e, h;
+            int d,s ;
+            
+            var sum = 0;
+            h = n / 1000;
+            s= n % 1000 / 100;
+            d= n % 100/10;
+            e = n % 10;
+            do
+            {
+                sum = sum + (n % 10);
+                n = n / 10;
+            }
+            while (n > 0);
+           
+             Console.WriteLine("h=" + h);
+           Console.WriteLine("s=" + s); 
+           Console.WriteLine("d=" + d);
+            Console.WriteLine("e=" + e);
+
+
+
+        }
+    }
+}
---
+++ факториел/факториел/факториел.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace факториел
+{
+    internal class факториел
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            var n= int.Parse(Console.ReadLine());
+            var P = 1;
+            while (n > 1) 
+            { 
+                P = P*n;
+                n--;
+            }
+            Console.WriteLine("P= " + P);
+
+
+
+        }
+    }
+}
